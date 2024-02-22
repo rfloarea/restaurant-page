2226c0bbd269eeba3a7e3d82f48fb7ec0157145d
rfloarea <account@rfloarea.net>
2024-02-22 15:55:58 -0500

    script to generate basic header component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('it works!!');
+
+// create our header component: nav, three buttons: home, about, menu
+const nav = document.querySelector('nav');
+
+const homeBtn = document.createElement('button');
+homeBtn.textContent = "Home";
+nav.appendChild(homeBtn);
+
+const aboutBtn = document.createElement('button');
+aboutBtn.textContent = "About";
+nav.appendChild(aboutBtn);
+
+const menuBtn = document.createElement('button');
+menuBtn.textContent = "Menu";
+nav.appendChild(menuBtn);
