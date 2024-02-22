8e962a657bab84d03050bacd64fcd91716a35b16
rfloarea <account@rfloarea.net>
2024-02-22 16:01:13 -0500

    add title and tagline

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('it works!!');
 
-// create our header component: nav, three buttons: home, about, menu
+// header component: nav container and three buttons/tabs: home, about, menu
 const nav = document.querySelector('nav');
 
 const homeBtn = document.createElement('button');
@@ -14,3 +14,14 @@ nav.appendChild(aboutBtn);
 const menuBtn = document.createElement('button');
 menuBtn.textContent = "Menu";
 nav.appendChild(menuBtn);
+
+// content component: title, description, image
+const content = document.querySelector('#content');
+
+const title = document.createElement('h1');
+title.textContent = "BEST RESTAURANT NEAR ME";
+content.appendChild(title);
+
+const tagLine = document.createElement('p');
+tagLine.textContent= "The number one destination among people searching the web for food.";
+content.appendChild(tagLine);
