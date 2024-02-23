89ed4abd1f5b5c57695364f0573de4d8246649d6
rfloarea <account@rfloarea.net>
2024-02-22 20:40:47 -0500

    clear content on tab click

--- src/index.js
+++ src/index.js
@@ -6,29 +6,32 @@ import { buildMenuPage } from './menu.js';
 
 window.addEventListener('load', buildHomepage);
 
+
+
+function removeAllChildNodes() {
+    const container = document.querySelector('#content');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+};
+
 // header component: nav container and three buttons/tabs: home, about, menu
 const nav = document.querySelector('nav');
 
 const homeBtn = document.createElement('button');
 homeBtn.textContent = "Home";
 nav.appendChild(homeBtn);
+homeBtn.addEventListener('click', removeAllChildNodes);
 homeBtn.addEventListener('click', buildHomepage);
-// homeBtn.addEventListener('click', clearContent(buildHomepage)) // clear page
 
 const aboutBtn = document.createElement('button');
 aboutBtn.textContent = "About";
 nav.appendChild(aboutBtn);
+aboutBtn.addEventListener('click', removeAllChildNodes);
 aboutBtn.addEventListener('click', buildAboutPage);
-// aboutBtn.addEventListener('click', ) // clear page
 
 const menuBtn = document.createElement('button');
 menuBtn.textContent = "Menu";
 nav.appendChild(menuBtn);
+menuBtn.addEventListener('click', removeAllChildNodes);
 menuBtn.addEventListener('click', buildMenuPage);
-
-
-// const container = document.querySelector('#content');
-
-// function clearContent(page) {
-//     container.replaceChildren(page);
-// };
